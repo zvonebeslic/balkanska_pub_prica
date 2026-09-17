f10c6e6ab8fc6c7edc54062ee0559d6f9582ddb1
zvonebeslic <besliczvonimir801@gmail.com>
2026-09-17 22:50:28 +0200

    Restore quiz initialization after percentile regression

--- daily30.js
+++ daily30.js
@@ -1,6 +1,10 @@
 /* KvizToGo loader: zadrzava Dnevnih 30 i ABC integraciju prije glavnog online-kviz koda. */
 document.write('<script src="daily30-core.js?v=20260917-1"><\/script><script src="abc-integration.js?v=20260917-1"><\/script>');
 
+/* Kompatibilnost sa starim online-kviz kodom: updateBlitzPercentileNote koristi
+   globalni note. Bez njega INIT puca prije loadQuestionsFromJson(). */
+var note = document.getElementById('blitz-percentile-note');
+
 /* Glavni online-kviz JS sam inicijalizira gumbe i ucitava JSON pitanja.
    Ovdje ne pozivamo njegove init funkcije drugi put jer bi se handleri duplirali. */
 
