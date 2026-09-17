027b2be2883ba4997cd02f4f0f3173328fcee0bd
zvonebeslic <besliczvonimir801@gmail.com>
2026-09-17 22:33:28 +0200

    Restore verified working quiz buttons and ABC integration

--- daily30.js
+++ daily30.js
@@ -1,14 +1,2 @@
-/* KvizToGo loader: ucitava postojeci Dnevnih 30 kod. ABC integracija je sada izravno u online-kviz.html, pa je ovdje ne ucitavamo drugi put. */
-document.write('<script src="daily30-core.js?v=20260917-1"><\/script>');
-
-/* ABC UI safety fix: online-kviz JavaScript ocekuje #abc-answer-options.
-   Ako ga HTML jos nema, dodaj ga uz postojeci unos odgovora prije pokretanja glavnog inline koda. */
-(function ensureAbcAnswerOptions(){
-  if (document.getElementById('abc-answer-options')) return;
-  const answerRow = document.querySelector('.answer-wrap .answer-input-row');
-  if (!answerRow) return;
-  const container = document.createElement('div');
-  container.className = 'abc-answer-options';
-  container.id = 'abc-answer-options';
-  answerRow.insertAdjacentElement('afterend', container);
-})();
+/* KvizToGo loader: zadržava postojeći Dnevnih 30 kod i dodaje ABC integraciju prije glavnog inline koda. */
+document.write('<script src="daily30-core.js?v=20260917-1"><\/script><script src="abc-integration.js?v=20260917-1"><\/script>');
